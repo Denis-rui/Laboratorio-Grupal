f21d62d111170ea3ed202282ade635fe578e9f09
Denis_Ruiz <denisruime.20@gmail.com>
2026-06-09 19:41:27 -0500

    crear: se creo el middlewar

---
+++ Libraries/Core/Http/MiddlewareDispatcher.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Core\Http;
+
+require_once __DIR__ . '/Interfaces.php';
+
+final class MiddlewareDispatcher implements RequestHandlerInterface
+{
+    private array $middlewares = [];
+    private RequestHandlerInterface $fallbackHandler;
+
+    public function __construct(RequestHandlerInterface $fallbackHandler)
+    {
+        $this->fallbackHandler = $fallbackHandler;
+    }
+
+    public function add(MiddlewareInterface $middleware): self
+    {
+        $this->middlewares[] = $middleware;
+        return $this;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $pipeline = array_reduce(
+            array_reverse($this->middlewares),
+            function (callable $nextStack, MiddlewareInterface $middleware): callable {
+                return function (ServerRequestInterface $req) use ($nextStack, $middleware): ResponseInterface {
+                    $wrapper = new class($nextStack) implements RequestHandlerInterface {
+                        private $next;
+
+                        public function __construct(callable $next)
+                        {
+                            $this->next = $next;
+                        }
+
+                        public function handle(ServerRequestInterface $request): ResponseInterface
+                        {
+                            return ($this->next)($request);
+                        }
+                    };
+
+                    return $middleware->process($req, $wrapper);
+                };
+            },
+            fn(ServerRequestInterface $req): ResponseInterface => $this->fallbackHandler->handle($req)
+        );
+
+        return $pipeline($request);
+    }
+}
