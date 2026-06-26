2573412562956683cd6a89103623561adb0bb1ae
Dante Delgado <smithdelgado111@gmail.com>
2026-06-26 13:06:21 -0500

    implementacion del enpoint json de atributos segun categoria

--- Config/Config.php
+++ Config/Config.php
@@ -1,8 +1,8 @@
 <?php
 
-const BASE_URL = "http://localhost/DAW-II/unidad-II/semana-6/";
+const BASE_URL = "http://localhost/laboratorio-grupal/";
 
-const CONNECTION = false; // Cambia a true para usar la base de datos
+const CONNECTION = true; // Cambia a true para usar la base de datos
 const BD_HOST = "localhost";
 const DB_PORT = "3306";
 const BD_NAME = "proyecto_php";
