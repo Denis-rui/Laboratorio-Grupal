b18507844bca81ae10a95627778f042ef40dd5ea
Denis_Ruiz <denisruime.20@gmail.com>
2026-06-26 13:47:48 -0500

    ya uni mi parte con la de mis compañeros[

--- Views/Categorias/listado.php
+++ Views/Categorias/listado.php
@@ -1,26 +1,5 @@
 <?php $data = $data ?? []; ?>
 
-<h2>Listado de Productos</h2>
-<a href="<?= BASE_URL ?>Productos/crear">Nuevo Producto</a>
-
-<!-- Agregamos un formulario para los filtros -->
-<div style="margin-bottom: 20px; padding: 15px; border: 1px solid #ccc;">
-    <label for="filtro_categoria">Categoria:</label>
-    <select id="filtro_categoria" name="filtro_categoria">
-        <option value="">-- Seleccione una categoría --</option>
-        <?php 
-        require_once "Models/CategoriasModel.php";
-            $categoriasModel = new CategoriasModel();
-            foreach ($categoriasModel->obtenerCategorias() as $cat):
-            ?>
-                <option value="<?= $cat['id'] ?>"><?= htmlspecialchars($cat['nombre']) ?></option>
-            <?php endforeach; ?>
-        </select>
-
-        <div id="contenedor-filtros" style="margin-top: 10px;"></div>
-</div>
-
-
 <h2>Listado de Categorias</h2>
 <a href="<?= BASE_URL ?>categorias/crear">Nueva Categoria</a>
 <table border="1">
